094d0432ef3ffee7390e941b0656d8813653bafb
jingyipotato <thexchanted@gmail.com>
2026-01-03 12:31:46 +0800

    feat: add liked articles by topic db logic

--- lib/data/deutsch.ts
+++ lib/data/deutsch.ts
@@ -6,3 +6,15 @@ export async function getLikedArticles(supabase: SupabaseClient) {
     .select("id, topic, completed_at")
     .eq("feedback", "up");
 }
+
+export async function getLikedcArticlesbyTopic(
+  supabase: SupabaseClient,
+  topic: string
+) {
+  return supabase
+    .from("deutsch")
+    .select("id, title_en, completed_at, sentiment, urgency, article_url")
+    .eq("feedback", "up")
+    .eq("topic", topic)
+    .order("completed_at", { ascending: true, nullsFirst: true });
+}
